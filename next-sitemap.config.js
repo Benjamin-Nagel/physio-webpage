3282c394ebe8748fc97042c238b0ba387949ae59
Benjamin Nagel <215310680+Benjamin-Nagel@users.noreply.github.com>
2025-10-24 17:42:54 +0200

    fixed sitemap configuration

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,11 +1,26 @@
 /** @type {import('next-sitemap').IConfig} */
+// process.env.PAGE_URL
 module.exports = {
-	generateRobotsTxt: true,
 	// Wherever are your pages stored
 	pagesDirectory: `${__dirname}/src/pages`,
+	targetDirectory: `${__dirname}/out`,
+	trailingSlash: true,
+
 	siteUrl: process.env.PAGE_URL
 		? `${process.env.PAGE_URL}`
 		: "https://my-fallback-url.com/",
-	targetDirectory: `${__dirname}/out`,
-	trailingSlash: true,
-};
+  changefreq: 'weekly',
+  priority: 0.7,
+  sitemapSize: 5000,
+  generateRobotsTxt: true,
+  exclude: [],
+  alternateRefs: [],
+  robotsTxtOptions: {
+    policies: [
+      {
+        userAgent: '*',
+        allow: '/',
+      },
+    ],
+  },
+}
